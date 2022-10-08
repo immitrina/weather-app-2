047cddf40127d5348fe905ac3e9857cc39d5f068
immitrina <amie.goodwin@yahoo.com>
2022-10-08 18:12:12 -0400

    Updated Secondary Weather Parameters

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ function displayTemperature(response) {
   let tempElement = document.querySelector("#temperature");
   let locationElement = document.querySelector("#location");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   tempElement.innerHTML = Math.round(response.data.main.temp);
   locationElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "6048019de1a84df24f7d81b812b1b98f";
